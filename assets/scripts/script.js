12735def97baf5c864bfceb86e2151414f11e730
sergTesst <kycok1@yahoo.com.ua>
2021-06-16 21:50:29 +0300

    removed old styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -28,3 +28,65 @@ function updateDisplay(){
     const likesSpan = document.getElementById("likeCounter");
     likesSpan.innerHTML = sessionStorage.getItem("likesCount");
 };
+
+
+document.querySelector('#labs-check').addEventListener('click', (event)=>{
+    const divLabsCont = document.querySelector('.labs-content');
+    // console.log(divLabsCont.style.display,
+    //      divLabsCont.style.height,
+    //      divLabsCont.style.fontSize
+    //      )
+    if(
+        divLabsCont.style.fontSize==''
+    ){
+        divLabsCont.style.fontSize = 'medium'
+    }else{
+        divLabsCont.style.fontSize = ''
+    }
+    
+})
+
+const projContainers = document.querySelectorAll('.project-container');
+Array.prototype.forEach.call(projContainers,(projCont)=>{
+
+    const aEl = projCont.querySelector('a');
+    const aString = aEl.innerHTML;
+    const arrLength = Array.from(aString).length;
+    const width = arrLength*14;
+    const widthPx = `${width}px`;
+    projCont.style.width = widthPx;
+
+
+    aEl.addEventListener('mouseover',(event)=>{
+        if(window.screen.width>1204)
+            projCont.style.width='100%'
+    })
+    aEl.addEventListener('mouseout',(event)=>{
+        projCont.style.width=widthPx;
+    })
+
+})
+
+const LangConfigArray = [
+    {javascript:'#efd81d'},
+    {react:'#61dafb'},
+    {'asp.net core':'#592c8c'},
+    {'entity framework':'#652076'},
+    {swagger:'#6a9500'},
+    {docker:'#2391e6'},
+    {xunit:'black'},
+    {angular:'#c3002f'},
+    {vue:'#3fb27f'}
+];
+
+function setRightColorForElements(){
+    const timeLineCont = document.querySelector('.timeline');
+    const childNodes = timeLineCont.childNodes;
+
+    function traversThroughChildNodes(childNodes){
+        
+    }
+}
+
+
+
